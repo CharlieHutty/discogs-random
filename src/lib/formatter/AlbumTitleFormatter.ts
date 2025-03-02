32b4eaae4ded9336d94fd2bea726aa9c5865066b
Charlie Hutty <charliehutty@yahoo.co.uk>
2025-03-02 13:35:24 +0000

    Uses AlbumTitleFormatter for slug

---
+++ src/lib/formatter/AlbumTitleFormatter.ts
@@ -0,0 +1,5 @@
+export default function format(albumTitle: string) {
+    const lower: string = albumTitle.toLowerCase();
+    const punctionationLess: string = lower.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()'']/g,"");
+    return punctionationLess.replaceAll(' ', '-');
+}
