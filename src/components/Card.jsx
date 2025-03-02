edaada341723fe0c553df41c41ae99804fd17bfb
Charlie Hutty <charliehutty@yahoo.co.uk>
2025-03-02 13:34:55 +0000

    Uses dynamic route for albums

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useRouter } from "next/navigation";
+import AlbumTitleFormatter from '@/lib/formatter/AlbumTitleFormatter';
+
 
 const Card = ({ imageUrl, title, description }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/album/${AlbumTitleFormatter(title)}`);
+  };
+
   return (
-    <div className="max-w-sm transform overflow-hidden rounded-2xl shadow-md shadow-gray-600/50 transition-transform hover:scale-105">
+    <div className="max-w-sm transform overflow-hidden rounded-2xl shadow-md shadow-gray-600/50 transition-transform hover:scale-105" onClick={handleClick}>
       <img className="h-48 w-full object-cover" src={imageUrl} alt={title} />
       <div className="p-4">
         <h2 className="text-lg font-semibold">{title}</h2>
